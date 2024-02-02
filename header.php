6487e44b784ffaac822e37335fcfb9b7b6bf776f
Yogendra Negi <ysn.negi009@gmail.com>
2024-02-02 13:49:59 +0530

    updated branch header code

--- header.php
+++ header.php
@@ -17,34 +17,34 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <?php wp_head();
-     ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class('at-sticky-sidebar'); ?>>
-<?php
-if (function_exitsts('wp_body_open')){
-    wp_body_open();
-}
-else{
-    do_action('wp_body_open');
-    }
-?>
-<?php
-if(true===get_theme_mod('lawcraft_enable_preloader',true)){
-?>
+    <?php
+        if (function_exitsts('wp_body_open')) {
+            wp_body_open();
+        }
+        else {
+            do_action('wp_body_open');
+        }
+    ?>
 
-<!-- start preloader -->
-<div class="loader-wrapper lds-flickr">
-    <div id="pre-loader">
-        <div class="loader-pulse"></div>
-    </div>
-</div>
-<!-- end preloader -->
-<?php
-}
-?>
-<!-- header styles -->
-<?php
-do_action('lawcraft_action_header');
-?>
+    <?php
+        if(true===get_theme_mod('lawcraft_enable_preloader',true)) {
+            ?>
+                <!-- start preloader -->
+                <div class="loader-wrapper lds-flickr">
+                    <div id="pre-loader">
+                        <div class="loader-pulse"></div>
+                    </div>
+                </div>
+                <!-- end preloader -->
+            <?php
+        }
+    ?>
+
+    <!-- header styles -->
+    <?php
+        do_action('lawcraft_action_header');
+    ?>

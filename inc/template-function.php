4066fea8d29903473c9ecfc8c81574f7219edd5e
Priyanshu Bamania <priyanshubamania70@gmail.com>
2024-02-02 15:56:17 +0530

    updated

--- inc/template-function.php
+++ inc/template-function.php
@@ -11,11 +11,10 @@
 
  if (! function_exists('lawcraft_header_menu_styles')) :
     function lawcraft_header_menu_styles() {
-        {
-            get_template_part( 'inc/header-menu/content',esc_html(get_theme_mod('lawcraft_header_menu_style','style1')));
-        }
-        endif;
-        add_action( 'lawcraft_action_header', 'lawcraft_header_menu_styles' ); 
+        get_template_part( 'inc/header-menu/content',esc_html(get_theme_mod('lawcraft_header_menu_style','style1')));
     }
+ endif;
+    add_action( 'lawcraft_action_header', 'lawcraft_header_menu_styles' ); 
+    
 
 /*header closed*/ 

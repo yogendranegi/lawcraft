5d6ed6baf2e62ae75bfe577b76cddd1265c1db55
Priyanshu Bamania <priyanshubamania70@gmail.com>
2024-02-02 13:21:30 +0530

    header created

---
+++ header.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section
+ * 
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package lawcraft
+ */
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <?php wp_head();
+     ?>
+</head>
+
+<body <?php body_class('at-sticky-sidebar'); ?>>
+<?php
+if (function_exitsts('wp_body_open'))
+{
+    wp_body_open();
+}
+else
+{
+    do_action('wp_body_open');
+}
+?>
+<?php
+if(true===get_theme_mod('lawcraft_enable_preloader',true))
+{
+?>
+
+<!-- start preloader -->
+<div class="loader-wrapper lds-flickr">
+    <div id="pre-loader">
+        <div class="loader-pulse"></div>
+    </div>
+</div>
+<!-- end preloader -->
+<?php
+}
+?>
+<!-- header styles -->
+<?php
+do_action('lawcraft_action_header');
+?>

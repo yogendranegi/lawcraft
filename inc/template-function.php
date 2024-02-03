42a57f5722b93ca1612ed7f19083fcabb0a9cd0e
Himanshu Pharswan <himanshupharswan67@gmail.com>
2024-02-03 11:29:57 +0530

    new edit in footer

--- inc/template-function.php
+++ inc/template-function.php
@@ -27,36 +27,36 @@
 
 
  if (! function_exists( 'lawcraft_footer_copyrights' ) );
- function lawcraft_footer_copyrights() {
-    ?>
-        <div class="row">
-            <div class="copyrights">
-                <p>
-                    <?php
+    function lawcraft_footer_copyrights() {
+        ?>
+            <div class="row">
+                <div class="copyrights">
+                    <p>
+                        <?php
 
-                        if("" != esc_html(get_theme_mod( 'lawcraft_footer_copyright_text'))) {
-                            echo esc_html(get_theme_mod( 'lawcraft_footer_copyright_text'));
-                            if(get_theme_mod('blogson_en_footer_credits',true)) {
-                                ?><span><?php esc_html_e(' | Theme by ','lawcraft') ?><a href="<?php echo esc_url(LAWCRAFT_THEME_AUTH); ?>" target="_blank"><?php esc_html_e('Spiracle Themes','lawcraft') ?></a></span>
-                                <?php  
+                            if("" != esc_html(get_theme_mod( 'lawcraft_footer_copyright_text'))) {
+                                echo esc_html(get_theme_mod( 'lawcraft_footer_copyright_text'));
+                                if(get_theme_mod('blogson_en_footer_credits',true)) {
+                                    ?><span><?php esc_html_e(' | Theme by ','lawcraft') ?><a href="<?php echo esc_url(LAWCRAFT_THEME_AUTH); ?>" target="_blank"><?php esc_html_e('Spiracle Themes','lawcraft') ?></a></span>
+                                    <?php  
+                                }
                             }
-                        }
-                        else{
-                            echo data_i18n(
-                                /*translators: Copyright data format, see https://secure.php.net/date */
-                                _x( 'Y', 'copyright data format','lawcraft')
-                            );
-                            ?>
-                                <a href="<?php echo esc_url( home_url('/' ) ); ?>"><?php bloginfo('name' ); ?></a>
-                                <span><?php esc_html_e(' | Theme by ','lawcraft') ?><a href="<?php echo esc_url(LAWCRAFT_THEME_AUTH); ?>" target="_blank"><?php esc_html_e('Spiracle Themes','lawcraft') ?></a></span>
-                                <?php
-                        }
-                    ?>
-                <p>
+                            else{
+                                echo data_i18n(
+                                    /*translators: Copyright data format, see https://secure.php.net/date */
+                                    _x( 'Y', 'copyright data format','lawcraft')
+                                );
+                                ?>
+                                    <a href="<?php echo esc_url( home_url('/' ) ); ?>"><?php bloginfo('name' ); ?></a>
+                                    <span><?php esc_html_e(' | Theme by ','lawcraft') ?><a href="<?php echo esc_url(LAWCRAFT_THEME_AUTH); ?>" target="_blank"><?php esc_html_e('Spiracle Themes','lawcraft') ?></a></span>
+                                    <?php
+                            }
+                        ?>
+                    <p>
+                </div>
             </div>
-        </div>
-    <?php                        
- }
+        <?php                        
+    }
 endif;
 add_action( 'lawcraft_action_footer', 'lawcraft_footer_copyrights' );
     
